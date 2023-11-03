e1e0ef7187225a8c0fa704548eae276b8a597b0a
Marc Charles <marccharles243@gmail.com>
2023-11-02 22:09:11 -0400

    Rewrote countMessage function to take in object instead of array.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * @return {string[]} an array of flavors.
  * */
 function getUserFlavors() {
+
     //Prompt user for flavors and store into variable
     const flavors = prompt("Enter the flavors separated by a comma", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
 
@@ -13,15 +14,31 @@ function getUserFlavors() {
 }
 
 /**
+ * The getTotal function provides sums the values
+ * from the values in the provided object and returns
+ * the total.
+ * @param {object} object the object to be counted.
+ * @return {number} The total amount.
+ */
+
+function getTotal(object) {
+    let total = 0;
+    for (const i in object) {
+        total += object[i];
+    }
+    return total;
+}
+
+/**
  * Generates a message on the total amount of flavors
  * requested by the user.
- * @param {string[]} array an array of flavors.
+ * @param {object} object an array of flavors.
  * @return {string} generated message.
  * */
-
-function countMessage(array) {
-    return `You've provided ${array.length} flavors`
+function countMessage(object) {
+    return `You've provided ${getTotal(object)} flavors`;
 }
+
 /**
  * The objFlavors function takes in an array of flavors
  * provided by the user and returns an object showing
@@ -31,13 +48,13 @@ function countMessage(array) {
  * */
 function objFlavors(strArr) {
     let obj = {};
-    for (let i in strArr) {
+    for (const i in strArr) {
         //Assign to flavor variable for readability
         const flavor = strArr[i];
         //If is not defined begin with a count of 1.
         if (obj[flavor] === undefined) {
             obj[flavor] = 1;
-        //Else increment value by 1.
+            //Else increment value by 1.
         } else {
             obj[flavor] += 1;
         }
@@ -45,17 +62,16 @@ function objFlavors(strArr) {
     return obj;
 }
 
+//Assigning inputs to variables
 const flavorArray = getUserFlavors();
-
-console.log(countMessage(flavorArray));
-
 const flavorObject = objFlavors(flavorArray);
 
-console.log("The flavors you've requested are: ")
+//Displaying total amount from values.
+console.log(countMessage(flavorObject));
 
-//Iterate through key and values in flavorObject and print to screen.
+// Iterate through key and values in flavorObject and print to screen.
+console.log("The flavors you've requested are: ")
 for (const key in flavorObject) {
     console.log(`${flavorObject[key]} - ${key} froyo(s)`);
 }
-
 console.log(flavorObject);
